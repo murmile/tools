738ed59f017a5fd00c7185d0f09c6ad158e5810d
koflerl64 <luk.kofler@univie.ac.at>
2025-12-18 16:13:50 +0100

    added clockedit function

--- clock/clock.sh
+++ clock/clock.sh
@@ -10,6 +10,7 @@
 # alias clockout='function _clockout() { [PATH_TO_SCRIPT]/clock.sh out "$@"; }; _clockout'
 # alias clock='function _clock() { [PATH_TO_SCRIPT]/clock.sh }; _clock'
 # alias clockopen='function _clockopen() { [PATH_TO_SCRIPT]/clock.sh open; }; _clockopen'
+# alias clockedit='function _clockedit() { [PATH_TO_SCRIPT]/clock.sh edit; }; _clockedit'
 
 shopt -s extglob  # Enable extended globbing
 
@@ -27,34 +28,35 @@ if [[ $1 ]]; then
 			clocker=$(date +"%Y-%m-%d $2:00")
 		elif [[ $2 == ++([0-9]) ]]; then
 			timediff=$(echo "$2" | grep -o '[0-9]\+')	
-            clocker=$(date -j -v +"$timediff"M +"%Y-%m-%d %H:%M:%S")
-        elif [[ $2 == -+([0-9]) ]]; then
-            timediff=$(echo "$2" | grep -o '[0-9]\+')	
+            		clocker=$(date -j -v +"$timediff"M +"%Y-%m-%d %H:%M:%S")
+        	elif [[ $2 == -+([0-9]) ]]; then
+            		timediff=$(echo "$2" | grep -o '[0-9]\+')	
 			clocker=$(date -j -v -"${timediff}"M +"%Y-%m-%d %H:%M:%S")
-        else
-            echo "### invalid parameters ###"
+        	else
+            		echo "### invalid parameters ###"
 			echo "# ------------- clock usage: ------------- #"
-		    echo "clockin [hh:mm]|[+|- mm] - clock in at specified OR current time +/- x min"
-		    echo "clockout [hh:mm]|[+|- mm] - clock out at specified OR current time +/- x min"
-		    echo "clock - show clock status"
+			echo "clockin [hh:mm]|[+|- mm] - clock in at specified OR current time +/- x min"
+			echo "clockout [hh:mm]|[+|- mm] - clock out at specified OR current time +/- x min"
+			echo "clock - show clock status"
 			echo "clockopen - open clock.log file"
 			echo ""
 			echo "clockin          # example 1: clock in at current time"
 			echo "clockin 08:30    # example 2: clock in at 08:30"
 			echo "clockout -10     # example 3: clock out 10 minutes ago"
 			echo "# ---------------------------------------- #"
-            exit 1
-        fi
+            		exit 1
+        	fi
 				
-    else
-        clocker=$(date +"%Y-%m-%d %H:%M:%S")
-    fi
+	else
+        	clocker=$(date +"%Y-%m-%d %H:%M:%S")
+	fi
     
 	# If clocking "in"
 	if [[ "$1" == "in" ]]; then
-    	echo "#clockin $clocker"
-    	echo "#clockin $clocker" >> "$SCRIPT_DIR/clock.log"
-		
+    		echo "#clockin $clocker"
+    		echo "#clockin $clocker" >> "$SCRIPT_DIR/clock.log"
+		exit 1
+	
 	# If clocking "out"
 	elif [[ "$1" == "out" ]]; then
 		
@@ -62,20 +64,20 @@ if [[ $1 ]]; then
 	    
 		### Calculate working hours between clock events
 		
-	    last=$(tail -n 1 "$SCRIPT_DIR/clock.log" | xargs)
+	    	last=$(tail -n 1 "$SCRIPT_DIR/clock.log" | xargs)
 			
 			# Confirming a preceding clock "in" event
-	    if [[ "$last" == *"in"* ]]; then
-	        lastclockin="${last#*#clockin }"
+	    	if [[ "$last" == *"in"* ]]; then
+	        	lastclockin="${last#*#clockin }"
 			lastclockin="${lastclockin//\[\] /}"
-	        lastclockintime=$(date -j -f "%Y-%m-%d %H:%M:%S" "$lastclockin" +"%s")
-	        clockouttime=$(date -j -f "%Y-%m-%d %H:%M:%S" "$clocker" +"%s")
-	        workminutes=$(( (clockouttime - lastclockintime) / 60 ))  # convert seconds to minutes
-	    else
-	        echo "warning: no prior clock-in"
+	        	lastclockintime=$(date -j -f "%Y-%m-%d %H:%M:%S" "$lastclockin" +"%s")
+	        	clockouttime=$(date -j -f "%Y-%m-%d %H:%M:%S" "$clocker" +"%s")
+	        	workminutes=$(( (clockouttime - lastclockintime) / 60 ))  # convert seconds to minutes
+	    	else
+	        	echo "warning: no prior clock-in"
 			echo "clock-in missing" >> "$SCRIPT_DIR/clock.log"
 			workminutes=0
-	    fi
+	    	fi
 	
 		hours=$(( workminutes / 60 ))
 		minutes=$(( workminutes % 60 ))
@@ -83,26 +85,33 @@ if [[ $1 ]]; then
 		
 		###
 
-	    echo "#clockout $clocker (hours worked: $workingtime)"
-	    echo "#clockout $clocker (hours worked: $workingtime)" >> "$SCRIPT_DIR/clock.log"
+	    	echo "#clockout $clocker (hours worked: $workingtime)"
+	    	echo "#clockout $clocker (hours worked: $workingtime)" >> "$SCRIPT_DIR/clock.log"
 	
-	# If simply opening time tracking .txt file
+		exit 1
+
+	# If editing .log file
+	elif [[ "$1" == "edit" ]]; then
+		vi "$SCRIPT_DIR/clock.log"
+		exit 1		
+	
+	# If opening .log file for viewing
 	elif [[ "$1" == "open" ]]; then
 		open "$SCRIPT_DIR/clock.log"
-		
+		exit 1
 	else
-        echo "### invalid parameters ###"
+        	echo "### invalid parameters ###"
 		echo "# ------------- clock usage: ------------- #"
-	    echo "clockin [hh:mm]|[+|- mm] - clock in at specified OR current time +/- x min"
-	    echo "clockout [hh:mm]|[+|- mm] - clock out at specified OR current time +/- x min"
-	    echo "clock - show clock status"
+	    	echo "clockin [hh:mm]|[+|- mm] - clock in at specified OR current time +/- x min"
+	    	echo "clockout [hh:mm]|[+|- mm] - clock out at specified OR current time +/- x min"
+	    	echo "clock - show clock status"
 		echo "clockopen - open clock.log file"
 		echo ""
 		echo "clockin          # example 1: clock in at current time"
 		echo "clockin 08:30    # example 2: clock in at 08:30"
 		echo "clockout -10     # example 3: clock out 10 minutes ago"
 		echo "# ---------------------------------------- #"
-        exit 1
+        	exit 1
 	fi
 
 # If no parameters, show current clock status and info
@@ -128,7 +137,6 @@ elif [[ -z "$1" ]]; then
 		hours=$(( workminutes / 60 ))
         	minutes=$(( workminutes % 60 ))
         	workingtime=$(printf "%d:%02d" "$hours" "$minutes")
-	
 		echo "currently clocked in (working time: $workingtime)"
 
     	elif [[ $last == *"out"* ]]; then
